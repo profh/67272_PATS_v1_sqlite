71e9e6922ea9591bea510c72ffbacefc2df22afd
profh <profh@cmu.edu>
2018-02-17 11:38:51 -0500

    Changes for midterm only -- do not merge into master

--- test/sets/pets.rb
+++ test/sets/pets.rb
@@ -2,9 +2,9 @@ module Contexts
   module Pets
 
     def create_pets
-      @dusty = FactoryBot.create(:pet, animal: @cat, owner: @alex, female: false)
-      @polo = FactoryBot.create(:pet, animal: @cat, owner: @alex, name: "Polo", active: false)
-      @pork_chop = FactoryBot.create(:pet, animal: @dog, owner: @mark, name: "Pork Chop")
+      @dusty = FactoryBot.create(:pet, animal: @cat, owner: @alex, female: false, date_of_birth: 13.years.ago.to_date)
+      @polo = FactoryBot.create(:pet, animal: @cat, owner: @alex, name: "Polo", active: false, date_of_birth: 3.years.ago.to_date)
+      @pork_chop = FactoryBot.create(:pet, animal: @dog, owner: @mark, name: "Pork Chop", date_of_birth: 51.weeks.ago.to_date)
     end
     
     def destroy_pets
--- test/sets/visits.rb
+++ test/sets/visits.rb
@@ -2,8 +2,8 @@ module Contexts
   module Visits
 
     def create_visits
-      @visit1 = FactoryBot.create(:visit, pet: @dusty)
-      @visit2 = FactoryBot.create(:visit, pet: @polo, date: 5.months.ago.to_date)
+      @visit1 = FactoryBot.create(:visit, pet: @dusty, weight: 9.5)
+      @visit2 = FactoryBot.create(:visit, pet: @polo, date: 5.months.ago.to_date, weight: 7.0)
       @visit3 = FactoryBot.create(:visit, pet: @polo, date: 2.months.ago.to_date)    
     end
     
